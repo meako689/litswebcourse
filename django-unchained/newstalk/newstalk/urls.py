ac736c073d35010e963197e8215738c8f7608420
Lits <lessons@litscourse>
2018-01-29 18:51:37 +0200

    Added views, urls and templates

--- django-unchained/newstalk/newstalk/urls.py
+++ django-unchained/newstalk/newstalk/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import NewsList
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', NewsList.as_view())
 ]
