611de039d76f7e7421ddae0c76911bad38f06281
Lits <lessons@litscourse>
2018-01-30 21:42:44 +0200

    Use model serializer. upd requirements.

--- django-unchained/newstalk/core/serializers.py
+++ django-unchained/newstalk/core/serializers.py
@@ -1,20 +1,8 @@
 from rest_framework import serializers
 from .models import NewsItem
 
-class NewsItemSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=150)
-    description = serializers.CharField(allow_blank=True, required=False)
-    link = serializers.CharField(max_length=450, allow_blank=True, required=False)
-    picture = serializers.CharField(max_length=450, allow_blank=True, required=False)
-
-    def create(self, validated_data):
-        return NewsItem.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data['title']
-        instance.description = validated_data['description']
-        instance.link = validated_data['link']
-        instance.picture = validated_data['picture']
-        instance.save()
+class NewsItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsItem
+        fields = ('id','title', 'picture', 'description', 'link')
 
