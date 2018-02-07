f5121443c9b0018e0a26267238e0df001807a69c
Lits <lessons@litscourse>
2018-02-07 18:29:10 +0200

    views

--- django-unchained/newstalk/core/views.py
+++ django-unchained/newstalk/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.generic import ListView
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 
 from .models import NewsItem
@@ -12,27 +14,32 @@ from .serializers import NewsItemSerializer
 class NewsList(ListView):
     model = NewsItem
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def api_news_list(request):
     if request.method == 'GET':
         items = NewsItem.objects.all().order_by('-date')
         serializer = NewsItemSerializer(items, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = NewsItemSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-@csrf_exempt
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def api_news_detail(request, id):
-    item = NewsItem.objects.get(id=id)
+    try:
+        item = NewsItem.objects.get(id=id)
+    except NewsItem.DoesNotExist:
+        return HttpResponse(status=404)
+
     if request.method == 'GET':
         serializer = NewsItemSerializer(item)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     if request.method == 'PUT':
         data = JSONParser().parse(request)
@@ -40,8 +47,8 @@ def api_news_detail(request, id):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=400)
 
     if request.method == 'DELETE':
         item.delete()
-        return JsonResponse({}, status=204)
+        return HttpResponse(status=204)
