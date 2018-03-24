cd5a67e0e4d7880c93b3e953a06965fa3ed1271a
Lits <lessons@litscourse>
2018-03-24 11:26:49 +0200

    settings

--- django-unchained/newstalk/deploy.sh
+++ django-unchained/newstalk/deploy.sh
@@ -1,7 +1,2 @@
 python manage.py migrate;
 python manage.py fetchnews;
-
-cd newsfront;
-npm install;
-npm run build;
-cd -;
--- django-unchained/newstalk/newstalk/settings.py
+++ django-unchained/newstalk/newstalk/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FRONTEND_DIR = os.path.join(BASE_DIR, 'newsfront/build')
+FRONTEND_DIR = os.path.join(BASE_DIR, 'build')
 
 STATICFILES_DIRS = [
     os.path.join(FRONTEND_DIR, "static"),
