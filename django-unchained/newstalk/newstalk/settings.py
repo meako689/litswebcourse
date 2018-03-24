5c95104d37e09d323019435b5896cc055fffc56d
Lits <lessons@litscourse>
2018-03-24 11:11:00 +0200

    fix

--- django-unchained/newstalk/newstalk/settings.py
+++ django-unchained/newstalk/newstalk/settings.py
@@ -100,14 +100,6 @@ CHANNEL_LAYERS = {
     },
 }
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
 
 
 # Password validation
--- django-unchained/newstalk/newstalk/urls.py
+++ django-unchained/newstalk/newstalk/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 from core.views import index
 
@@ -22,5 +22,5 @@ from core.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.api_urls')),
-    path(r'^.*/$', index),
+    re_path(r'^.*/$', index),
 ]
